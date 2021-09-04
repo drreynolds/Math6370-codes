a46d3da599dea24cd4cb542bd943124a3fb74520
Daniel R. Reynolds <reynolds@smu.edu>
2021-09-04 14:03:01 -0500

    Initial import of serial dot-product code

---
+++ dot_prod/dot_prod.cpp
@@ -0,0 +1,68 @@
+/* Daniel R. Reynolds
+   SMU Mathematics
+   Math 4370/6370
+   7 February 2015 */
+
+
+// Inclusions
+#include <stdlib.h>
+#include <stdio.h>
+#include "get_time.h"
+
+
+// Example routine to compute the dot-product of two vectors
+int main(int argc, char* argv[]) {
+
+  // declarations
+  int i, n;
+  double *a, *b, sum, alloctime, inittime, runtime;
+  double stime, ftime;
+  
+  // ensure that an argument was passed in
+  if (argc < 2) {
+    printf("Error: function requires one argument (vector length)\n");
+    return 1;
+  }
+
+  // set n as the input argument, and ensure it's positive
+  n = atoi(argv[1]);
+  if (n < 1) {
+    printf("Error: vector length %i must be greater than 0\n", n);
+    return 1;
+  }
+
+  // allocate the vectors
+  stime = get_time();
+  a = new double[n];
+  b = new double[n];
+  ftime = get_time();
+  alloctime = ftime-stime;
+
+  // initialize the vector values
+  stime = get_time();
+  for (i=0; i<n; i++)    a[i] = (0.001 * (i + 1.0)) / n;
+  for (i=0; i<n; i++)    b[i] = (0.001 * (n - i - 1.0)) / n;
+  ftime = get_time();
+  inittime = ftime-stime;
+
+  // compute dot-product
+  stime = get_time();
+  sum = 0.0;
+  for (i=0; i<n; i++)   sum += a[i]*b[i];
+  ftime = get_time();
+  runtime = ftime - stime;
+
+  // output computed value and runtime
+  printf(" vector length = %i\n",n);
+  printf("   dot-product = %.16e\n",sum);
+  printf("    alloc time = %.2e\n",alloctime);
+  printf("     init time = %.2e\n",inittime);
+  printf("      run time = %.2e\n",runtime);
+
+  // delete vectors
+  delete[] a;
+  delete[] b;
+
+  return 0;
+} // end main
+
---
+++ dot_prod/dot_prod.f
@@ -0,0 +1,76 @@
+c=================================================================
+c     Daniel R. Reynolds
+c     SMU Mathematics
+c     Math 4370/6370
+c     7 February 2015
+c=================================================================
+
+
+      program DotProd
+c-----------------------------------------------------------------
+c     Example routine to compute the dot-product of two vectors.
+c-----------------------------------------------------------------
+c======= Inclusions ===========
+      
+c======= Declarations =========
+      implicit none
+
+      integer, parameter :: MAX_INTEGER_DIGITS = 10
+      integer :: i, n
+      character(MAX_INTEGER_DIGITS) :: n_string
+      double precision, allocatable :: a(:), b(:)
+      double precision :: sum, stime, ftime
+      double precision :: alloctime, inittime, runtime
+      
+c======= Internals ============
+
+c     get n from the command line
+      call getarg(1, n_string)
+
+c     ensure that an argument was passed in
+      if ( n_string == '' ) then
+         stop 'Error: function requires one argument (vector length)'
+      endif
+  
+c     convert n_string to integer, and ensure it's positive
+      read (n_string, *) n
+      if ( n < 1 ) then
+         stop 'Error: vector length must be greater than 0'
+      endif
+
+c     allocate the vectors
+      call get_time(stime)
+      allocate(a(n),b(n))
+      call get_time(ftime)
+      alloctime = ftime - stime
+
+c     initialize the vectors
+      call get_time(stime)
+      do i=1,n
+         a(i) = 1.d-3*(i)/n
+         b(i) = 1.d-3*(n-i)/n
+      enddo
+      call get_time(ftime)
+      inittime = ftime - stime
+
+c     compute dot-product
+      call get_time(stime)
+      sum = 0.d0
+      do i=1,n,1
+         sum = sum + a(i)*b(i)
+      enddo
+      call get_time(ftime)
+      runtime = ftime-stime
+
+c     output computed value and runtime 
+      print *, ' vector length =',n
+      print *, '   dot-product =',sum
+      print *, '    alloc time =',alloctime
+      print *, '     init time =',inittime
+      print *, '      run time =',runtime
+      
+c     free vectors
+      deallocate(a,b)
+
+      end program DotProd
+c=================================================================
