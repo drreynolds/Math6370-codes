c89d03d45bf48e23f245b4c2a5fd6c9dd0b54a13
Daniel R. Reynolds <reynolds@smu.edu>
2021-10-25 22:24:31 -0500

    Updated pi_comp example to call RAJA_DEVICE function within loop

--- axpy/src/axpy.cpp
+++ axpy/src/axpy.cpp
@@ -30,31 +30,14 @@ int main(int argc, char* argv[]) {
 
   // allocate the vectors
   double *x, *y, *z;
-#if defined(RAJA_ENABLE_CUDA)
   cudaMallocManaged((void**)&x, n*sizeof(double), cudaMemAttachGlobal);
   cudaMallocManaged((void**)&y, n*sizeof(double), cudaMemAttachGlobal);
   cudaMallocManaged((void**)&z, n*sizeof(double), cudaMemAttachGlobal);
-#else
-  x = new double[n];
-  y = new double[n];
-  z = new double[n];
-#endif
 
   // set the RAJA policies
-#if defined(RAJA_ENABLE_CUDA)
   using epolicy = RAJA::cuda_exec<256>;
   using rpolicy = RAJA::cuda_reduce;
   std::cout << "Running axpy with length " << n << " with RAJA using CUDA backend:\n";
-#elif defined(RAJA_ENABLE_OPENMP)
-  using epolicy = RAJA::omp_parallel_for_exec;
-  using rpolicy = RAJA::omp_reduce;
-  std::cout << "Running axpy with length " << n << " with RAJA using OpenMP backend:\n";
-#else
-  using epolicy = RAJA::seq_exec;
-  using rpolicy = RAJA::seq_reduce;
-  std::cout << "Running axpy with length " << n << " with RAJA using sequential backend:\n";
-#endif
-
 
   // start timer
   const std::chrono::time_point<std::chrono::system_clock> stime =
@@ -62,22 +45,22 @@ int main(int argc, char* argv[]) {
 
   // initialize a, x and y
   const double a = -3.0;
-  RAJA::forall<epolicy>(RAJA::RangeSegment(0,n), [=] RAJA_HOST_DEVICE (int i) {
+  RAJA::forall<epolicy>(RAJA::RangeSegment(0,n), [=] RAJA_DEVICE (int i) {
     x[i] = exp(2.0*(i+1)/n);
     y[i] = 1.0*(n-1)/n;
   });
 
   // perform linear combinations
-  RAJA::forall<epolicy>(RAJA::RangeSegment(0,n), [=] RAJA_HOST_DEVICE (int i) {
+  RAJA::forall<epolicy>(RAJA::RangeSegment(0,n), [=] RAJA_DEVICE (int i) {
     z[i] = a*x[i] + y[i];});
-  RAJA::forall<epolicy>(RAJA::RangeSegment(0,n), [=] RAJA_HOST_DEVICE (int i) {
+  RAJA::forall<epolicy>(RAJA::RangeSegment(0,n), [=] RAJA_DEVICE (int i) {
     x[i] = y[i]/a - z[i];});
-  RAJA::forall<epolicy>(RAJA::RangeSegment(0,n), [=] RAJA_HOST_DEVICE (int i) {
+  RAJA::forall<epolicy>(RAJA::RangeSegment(0,n), [=] RAJA_DEVICE (int i) {
     y[i] = x[i]*y[i]/n;});
 
   // output maximum value in z
   RAJA::ReduceMax<rpolicy, double> zmax(-1e100);
-  RAJA::forall<epolicy>(RAJA::RangeSegment(0,n), [=] RAJA_HOST_DEVICE (int i) {
+  RAJA::forall<epolicy>(RAJA::RangeSegment(0,n), [=] RAJA_DEVICE (int i) {
     zmax.max(z[i]);});
   std::cout << "  max(z) = " << std::setprecision(16) << zmax.get() << std::endl;
 
