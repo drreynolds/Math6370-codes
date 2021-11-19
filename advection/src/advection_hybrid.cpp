2b5b2e1a66ef345344196bcf651f0606cba91718
Daniel R. Reynolds <reynolds@smu.edu>
2021-11-19 13:04:35 -0600

    Added hybrid RAJA+OpenMP version of advection code to separate I/O from computation

--- advection/src/advection_hybrid.cpp
+++ advection/src/advection_hybrid.cpp
@@ -5,6 +5,7 @@
 
 // Inclusions
 #include "advection.hpp"
+#include "omp.h"
 
 // Example routine to evolve the first-order 2D wave equations in time
 int main(int argc, char* argv[]) {
@@ -69,26 +70,44 @@ int main(int argc, char* argv[]) {
   ftime = std::chrono::system_clock::now();
   std::chrono::duration<double> inittime = ftime - stime;
 
-  // set initial time, output initial solution
-  double t = 0.0;
-  double toutput = 0.0;
-  int noutput = 0;
-  std::cout << "writing output file " << noutput << ", step = 0, t = " << t << std::endl;
-  stime = std::chrono::system_clock::now();
-  output(u_h, u_d, t, nx, ny, noutput);
-  ftime = std::chrono::system_clock::now();
-  iotime += (ftime - stime);
-
   // set time step
   double dt = (dx < dy) ? dx/c/50.0 : dy/c/50.0;
 
-  // start time stepping
-  int it;
-  std::chrono::duration<double> runtime;
-  for (it=0; it<nt; it++) {
+  // set initial time, output initial solution
+  int output_steps = ceil(dtoutput/dt);
+  int noutput = 0;
+  std::cout << "writing output file " << noutput << ", step = 0, t = 0" << std::endl;
+  stime = std::chrono::system_clock::now();
+  output(u_h, u_d, 0.0, nx, ny, noutput);
+  ftime = std::chrono::system_clock::now();
+  iotime += (ftime - stime);
 
+  // ensure exactly 2 OpenMP threads
+  omp_set_num_threads(2);
+
+  // create run timer, and global iteration counter
+  int numsteps=0;
+  std::chrono::duration<double> runtime;
+
+#pragma omp parallel default(shared)
+{
+  // determine thread number
+  int myid = omp_get_thread_num();
+
+  // output number of threads in the team
+#pragma omp master
+  std::cout << "Running with " << omp_get_num_threads() << " threads\n";
+
+  // start time stepping
+  for (int it=0; it<nt; it++) {
+
+#pragma omp master
+    {
     // start timer
     stime = std::chrono::system_clock::now();
+
+    // update global iteration counter
+    numsteps += 1;
 
     // first update v1 to get to half time step
     RAJA::kernel<xy_kernel_policy>(RAJA::make_tuple(RAJA::RangeSegment(0, ny),
@@ -134,35 +153,46 @@ int main(int argc, char* argv[]) {
       u_d[idx(i,j,nx)] += dt*v1_d[idx(i,j,nx)];
     });
 
-    // update time
-    t += dt;
+    // update runtime
     ftime = std::chrono::system_clock::now();
     runtime += ftime - stime;
+    }
+
+    // set current time
+    double t = (it+1)*dt;
 
     // stop simulation if we've reached tstop
     if (t >= tstop)  break;
 
     // output solution periodically
-    if ( t - (toutput + dtoutput) > -1.e-14 ) {
-      stime = std::chrono::system_clock::now();
-      toutput = t;
-      noutput++;
-      std::cout << "writing output file " << noutput << ", step = "
-                << it << ", t = " << t << std::endl;
-      output(u_h, u_d, t, nx, ny, noutput);
-      ftime = std::chrono::system_clock::now();
-      iotime += ftime - stime;
+    if ( (it+1)%output_steps == 0 ) {
+
+#pragma omp barrier
+      if (myid == 0) {
+        // copy device data to host
+        cudaMemcpy( u_h, u_d, nx*ny*sizeof(double), cudaMemcpyDeviceToHost);
+      }
+#pragma omp barrier
+      if (myid == 1) {
+        std::chrono::time_point<std::chrono::system_clock> iostime =  std::chrono::system_clock::now();
+        noutput++;
+        std::cout << "writing output file " << noutput << ", step = "
+                  << it << ", t = " << t << std::endl;        
+        output(u_h, u_d, t, nx, ny, noutput);
+        std::chrono::time_point<std::chrono::system_clock> ioftime = std::chrono::system_clock::now();
+        iotime += ioftime - iostime;
+      }
     }
 
   } // for it
+  } // omp parallel
 
   // output final solution
   stime = std::chrono::system_clock::now();
-  toutput = t;
   noutput++;
   std::cout << "writing output file " << noutput << ", step = "
-            << it << ", t = " << t << std::endl;
-  output(u_h, u_d, t, nx, ny, noutput);
+            << numsteps << ", t = " << numsteps*dt << std::endl;
+  output(u_h, u_d, nt*dt, nx, ny, noutput);
   ftime = std::chrono::system_clock::now();
   iotime += ftime - stime;
 
