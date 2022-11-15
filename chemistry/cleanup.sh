6e8161651ffe021c168865c3a3e77e32eee709f4
Daniel R. Reynolds <reynolds@smu.edu>
2022-11-15 13:54:42 -0600

    Added utility scripts for chemistry solver demo on behemoth

---
+++ chemistry/cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile chemistry.cuda chemistry.serial chemistry.openmp
---
+++ chemistry/runtests_behemoth.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# set desired number of chemical bins
+N=2000
+
+# set desired numbers of OpenMP threads
+THREADS=(1 2 4 8 16 32)
+
+# configure build
+cmake -DKokkos_DIR=/usr/local/kokkos-3.6.00/gcc-11.2.0/lib/cmake/Kokkos
+
+# build executables
+make
+
+# run serial test
+echo "  "
+echo "running serial version, chemistry.serial:"
+./chemistry.serial $N
+
+# run OpenMP tests
+for t in "${THREADS[@]}"
+do
+    echo "  "
+    echo "running OpenMP version, chemistry.openmp, with $t threads:"
+    OMP_NUM_THREADS=$t ./chemistry.openmp $N
+
+done
+
+# run CUDA test
+echo "  "
+echo "running CUDA version, chemistry.cuda:"
+./chemistry.cuda $N
