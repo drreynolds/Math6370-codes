42ef8f7e663d671c43c381385fcfa16ac66696ea
Daniel R. Reynolds <reynolds@smu.edu>
2022-11-15 16:15:30 -0600

    Minor update

--- advection/cleanup.sh
+++ advection/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile advection.cuda advection.serial advection.openmp advection.hybrid u_sol.* *~
+rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile advection.cuda advection.serial advection.openmp advection.hybrid u_sol.* *~ *.png
