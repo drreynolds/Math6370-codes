c163cf7090c5ff1a284052b4111aeb3371832871
Reynolds <reynolds@m3login03.cm.cluster>
2024-12-18 13:30:41 -0600

    created setup script for appropriate Maneframe3 modules

---
+++ HYPRE/setup_m3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# File to set the relevant environment variables to compile/run this example on Maneframe3
+
+module load hypre/2.31.0-wufze77
+export HYPRE_ROOT=.
+export MPICXX=mpicxx
