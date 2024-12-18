bfb6a7ee122a02b7ff5bbae8a1faf77a32c01919
Daniel Reynolds <reynolds@smu.edu>
2024-12-18 13:32:55 -0600

    Updated M3 setup script to include the full set of compatible modules

--- HYPRE/setup_m3.sh
+++ HYPRE/setup_m3.sh
@@ -2,6 +2,7 @@
 #
 # File to set the relevant environment variables to compile/run this example on Maneframe3
 
-module load hypre/2.31.0-wufze77
+module purge
+module load spack gcc/11.2.0 emacs cmake openmpi hypre/2.31.0-wufze77
 export HYPRE_ROOT=.
 export MPICXX=mpicxx
